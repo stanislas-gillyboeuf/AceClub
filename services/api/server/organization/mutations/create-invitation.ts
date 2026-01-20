99c1241f5b2fcb6feb2cae1b6b5688e865aca98a
Nicolas Becharat <nicolas.becharat@gmail.com>
2026-01-20 21:02:38 +0100

    feature/create-invitation-one-org

--- services/api/server/organization/mutations/create-invitation.ts
+++ services/api/server/organization/mutations/create-invitation.ts
@@ -3,16 +3,38 @@ import { HonoContext } from "../../../types/hono";
 import { z } from "zod";
 import { createInvitationValidator } from "../validators";
 import { auth } from "../../../auth";
+import { db } from "../../../db";
+import { organization, user } from "../../../db/schema";
+import { eq } from "drizzle-orm";
 
 export const createInvitation = async (c: Context<HonoContext>) => {
     try {
         // @ts-ignore
         const validated = c.req.valid("json") as z.infer<typeof createInvitationValidator>;
 
+
+        const checkInvitation = await db.query.invitation.findFirst({
+            where: eq(user.email, validated.email),
+        });
+
+        if (checkInvitation) {
+            return c.json({ error: "User already has an invitation" }, 400);
+        }
+
+        if (validated.organizationId) {
+            const checkOrganization = await db.query.organization.findFirst({
+                where: eq(organization.id, validated.organizationId),
+            });
+
+            if (!checkOrganization) {
+                return c.json({ error: "Organization not found" }, 404);
+            }
+        }
+
         const result = await auth.api.createInvitation({
             body: {
                 email: validated.email,
-                role: validated.role,
+                role: validated.role as "member" | "admin" | "owner",
                 organizationId: validated.organizationId,
                 resend: validated.resend,
             },
